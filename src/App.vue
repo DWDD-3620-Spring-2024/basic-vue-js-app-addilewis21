f16fc2a557aae20b2e7d8d3ce3d5bc9743699009
addilewis21 <addilewis21@gmail.com>
2024-04-09 10:27:41 -0600

    added back in my code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,143 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h1>Todo List</h1>
+<div class="app-container">
+  <div class="input-container">
+    <input type="text" v-model="newTodo" @keyup.enter="addTodo">
+    <button class="add-todo-button" @click="addTodo">Add Todo</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+</div>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index" :class="{ completed: todo.isCompleted }">
+        <div v-if="todo.isEditing">
+          <input type="text" v-model="todo.text" @keyup.enter="todo.isEditing = false">
+        </div>
+        <div v-else>
+          {{ todo.text }}
+          <button class="edit-button" @click="todo.isEditing = true">Edit</button>
+          <button class="complete-button" @click="todo.isCompleted = !todo.isCompleted">
+            {{ todo.isCompleted ? 'Uncomplete' : 'Complete' }}
+          </button>
+          <button class="remove-button" @click="removeTodo(index)">Remove</button>
+        </div>
+      </li>
+    </ul>
+    <p>{{ remaining }} todos left</p>
+    <button class="clear-button" @click="clearCompleted">Clear completed todos</button>
+  </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    }
+  },
+  computed: {
+    remaining() {
+      return this.todos.filter(todo => !todo.isCompleted).length;
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({ text: this.newTodo, isCompleted: false, isEditing: false });
+      this.newTodo = '';
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    clearCompleted() {
+      this.todos = this.todos.filter(todo => !todo.isCompleted);
+    }
+  }
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.app-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.input-container {
+  display: flex;
+
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+ul {
+  text-decoration: none;
+  }
+
+#app {
+  text-align: center;
+  padding: 10px;
+  width: 700px;
+  margin: auto;
+  background-color: lightgray;
+  
+}
+.clear-button {
+  border: solid 2px #f44336;
+  padding: 5px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 15px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+.add-todo-button {
+  border: solid 2px black;
+  height: 40px;
+  width: 170px;
+  margin: auto;
+  font-size: 15px;
+}
+.edit-button {
+  background-color: white;
+  border: solid 2px black;
+  padding: 4px 10px;
+  width: 60px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 15px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+.remove-button {
+  background-color: #f44336;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 15px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+.complete-button {
+  background-color: #008CBA;
+  color: white;
+  width: 100px;
+  border: none;
+  padding: 5px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 15px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+.completed {
+  text-decoration: line-through;
+  color: green;
+
 }
 </style>
+
